4a8630913569a71243a2f2024cfce06973c4e97d
poby123 <i2pir@naver.com>
2024-11-07 00:18:31 +0900

    suffix array

---
+++ 2022-autumn-adv/02/11479.cpp
@@ -0,0 +1,91 @@
+/**
+ * g++ --std=c++17 11479.cpp -o test
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> getSuffixArray(const string& s) {
+  int n = s.size();
+  vector<int> sa(n), isa(n), temp(n);
+
+  auto counting_sort = [&]() {
+    vector<int> count(max(256, n), 0);
+    for (int i = 0; i < n; i++) {
+      ++count[isa[i]];
+    }
+    partial_sum(count.begin(), count.end(), count.begin());
+    for (int i = n - 1; i >= 0; i--) {
+      sa[--count[isa[temp[i]]]] = temp[i];
+    }
+  };
+
+  for (int i = 0; i < n; i++) {
+    sa[i] = temp[i] = i;
+    isa[i] = s[i];
+  }
+
+  counting_sort();
+
+  for (int k = 1;; k <<= 1) {
+    int index = 0;
+    for (int i = n - k; i < n; i++) {
+      temp[index++] = i;
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (sa[i] >= k) {
+        temp[index++] = sa[i] - k;
+      }
+    }
+
+    counting_sort();
+    temp[sa[0]] = 0;
+
+    for (int i = 1; i < n; i++) {
+      temp[sa[i]] = temp[sa[i - 1]];
+      if (sa[i - 1] + k < n && sa[i] + k < n && isa[sa[i - 1]] == isa[sa[i]] &&
+          isa[sa[i - 1] + k] == isa[sa[i] + k]) {
+        continue;
+      }
+      ++temp[sa[i]];
+    }
+
+    swap(isa, temp);
+    if (isa[sa.back()] + 1 == n) {
+      break;
+    }
+  }
+
+  return sa;
+}
+
+vector<int> getLCPArray(string& s, vector<int>& sa) {
+  int N = s.length();
+  vector<int> lcp(N), isa(N);
+  for (int i = 0; i < N; i++) isa[sa[i]] = i;
+  for (int i = 0, k = 0; i < N; i++, k = max(k - 1, 0)) {
+    if (isa[i] == 0) continue;
+    for (int j = sa[isa[i] - 1]; s[i + k] == s[j + k]; k++);
+    lcp[isa[i]] = k;
+  }
+  return lcp;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+
+  int n = s.size();
+  auto suffixArray = getSuffixArray(s);
+  auto lcpArray = getLCPArray(s, suffixArray);
+  const auto sumOfLcpArray = accumulate(lcpArray.begin(), lcpArray.end(), 0LL);
+
+  const long long ans = (long long)n * (n + 1) / 2LL - sumOfLcpArray;
+  cout << ans << "\n";
+
+  return 0;
+}
--- test.cpp
+++ test.cpp
@@ -3,46 +3,87 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, R, Q;
-vector<vector<int>> G;
-vector<int> numberOfNode;
+using ll = long long;
+#define MAX 9
+using pp = pair<int, int>;
+vector<pp> blanks;
 
-int dfs(int node, int source) {
-  for (auto next : G[node]) {
-    if (next == source) {
+int N;
+int G[MAX][MAX] = {0};
+bool isDone;
+
+const int dy[] = {0, 0, 0, 1, 1, 1, 2, 2, 2};
+const int dx[] = {0, 1, 2, 0, 1, 2, 0, 1, 2};
+
+bool isAvailable(int n, int y, int x) {
+  int sy = y / 3 * 3;
+  int sx = x / 3 * 3;
+
+  for (int i = 0; i < MAX; i++) {
+    if (G[y][i] == n) {
+      return false;
+    }
+
+    if (G[i][x] == n) {
+      return false;
+    }
+
+    if (G[sy + dy[i]][sx + dx[i]] == n) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void solve(int i) {
+  if (i == blanks.size()) {
+    isDone = true;
+    for (int i = 0; i < MAX; i++) {
+      for (int j = 0; j < MAX; j++) {
+        cout << G[i][j];
+      }
+      cout << "\n";
+    }
+
+    return;
+  }
+
+  auto [y, x] = blanks[i];
+  for (int t = 1; t < 10; t++) {
+    if (isDone) {
+      return;
+    }
+
+    if (!isAvailable(t, y, x)) {
       continue;
     }
-    numberOfNode[node] += dfs(next, node);
+
+    G[y][x] = t;
+    solve(i + 1);
+    G[y][x] = 0;
   }
-  return numberOfNode[node];
 }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  blanks.reserve(80);
 
-  cin >> N >> R >> Q;
-  G.resize(N + 1);
-  numberOfNode.resize(N + 1, 1);
+  for (int i = 0; i < MAX; i++) {
+    string str;
+    getline(cin, str);
 
-  for (int i = 0; i < N; i++) {
-    G[i].reserve(4);
+    for (int j = 0; j < MAX; j++) {
+      G[i][j] = str[j] - '0';
+
+      if (!G[i][j]) {
+        blanks.push_back({i, j});
+      }
+    }
   }
 
-  for (int i = 0; i < N - 1; i++) {
-    int a, b;
-    cin >> a >> b;
-    G[a].push_back(b);
-    G[b].push_back(a);
-  }
-
-  dfs(R, 0);
-
-  for (int i = 0; i < Q; i++) {
-    int x;
-    cin >> x;
-    cout << numberOfNode[x] << "\n";
-  }
+  solve(0);
 
   return 0;
 }
