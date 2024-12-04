3b5ba758f103e6e6b8197699d44305657046d7e2
poby123 <50279318+poby123@users.noreply.github.com>
2024-12-05 00:19:27 +0900

    feat: 7579

--- test.cpp
+++ test.cpp
@@ -3,69 +3,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define MAX 1000000
 
-const int h = ((int)ceil(log2(MAX)));
-const int treeSize = (1 << (h + 1));
+#define MAX 101
 
-struct Seg
-{
-  vector<ll> tree = vector<ll>(treeSize, 0);
-  int sz = treeSize >> 1;
+int N, M;
+int dp_mem[MAX][MAX * MAX], mem[MAX], cost[MAX];
 
-  void update(int x, ll v)
-  {
-    x |= sz;
-    ll diff = v - tree[x];
-    tree[x] = v;
-    while (x >>= 1)
-    {
-      tree[x] = tree[x << 1] + tree[x << 1 | 1];
-    }
-  }
-
-  ll query(int l, int r)
-  {
-    l |= sz, r |= sz;
-    ll ret = 0;
-    while (l <= r)
-    {
-      if (l & 1)
-        ret += tree[l++];
-      if (~r & 1)
-        ret += tree[r--];
-      l >>= 1, r >>= 1;
-    }
-    return ret;
-  }
-} seg;
-
-int N, M, K;
-
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  cin >> N >> M >> K;
-  for (int i = 1; i <= N; i++)
-  {
-    ll x;
-    cin >> x;
-    seg.update(i, x);
+  cin >> N >> M;
+  for (int i = 1; i <= N; i++) {
+    cin >> mem[i];
   }
 
-  for (int i = 0; i < M + K; i++)
-  {
-    ll a, b, c;
-    cin >> a >> b >> c;
-    if (a == 1)
-    {
-      seg.update(b, c);
+  int sum_cost = 0;
+  for (int i = 1; i <= N; i++) {
+    cin >> cost[i];
+    sum_cost += cost[i];
+  }
+
+  for (int i = 1; i <= N; i++) {
+    for (int j = 0; j <= sum_cost; j++) {
+      if (j - cost[i] >= 0) {
+        dp_mem[i][j] = max(dp_mem[i][j], dp_mem[i - 1][j - cost[i]] + mem[i]);
+      }
+      dp_mem[i][j] = max(dp_mem[i][j], dp_mem[i - 1][j]);
     }
-    else
-    {
-      cout << seg.query(b, c) << "\n";
+  }
+
+  for (int j = 0; j <= sum_cost; j++) {
+    if (dp_mem[N][j] >= M) {
+      cout << j << "\n";
+      break;
     }
   }
 
